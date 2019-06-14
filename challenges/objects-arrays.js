ea358ce3b682839d4ec24487231fbfc8792b8c7d
Scott Renz <scottrenz@scottrenz.com>
2019-06-14 11:54:31 -0400

    made university array and sorted it

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -63,11 +63,12 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
-
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities)
 
+const universities = graduates.map(f=> {
+  return f.university})
+  universities.sort()
+  console.log(universities)
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
 The resulting contact information should have a space between the first name and the email information like this: 
